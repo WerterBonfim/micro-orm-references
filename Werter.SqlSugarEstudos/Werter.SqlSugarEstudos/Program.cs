814ddd884e5d8888085b3e3791c5c6abec50f68d
werter bonfim <werter@hotmail.com.br>
2021-12-28 18:26:45 -0300

    estudos SqlSugar

---
+++ Werter.SqlSugarEstudos/Werter.SqlSugarEstudos/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+using SqlSugar;
+
+Console.WriteLine("Hello, World!");
+
+
+var db = new SqlSugarClient(new ConnectionConfig
+{
+    DbType = DbType.SqlServer,
+    ConnectionString = "Server=localhost,1433;Database=ExperimentosDotnet;User Id=sa;Password=!123Senha;",
+
+});
