8e61201d5b419616089da966cd2887c991e24e7f
werter bonfim <werter@hotmail.com.br>
2021-12-28 20:29:10 -0300

    primeiros experimentos

---
+++ Werter.SqlSugarEstudos/Werter.SqlSugarEstudos/ModelosGerados/TB_PrimeiraTabela.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace Models
+{
+    ///<summary>
+    ///
+    ///</summary>
+    public partial class TB_PrimeiraTabela
+    {
+           public TB_PrimeiraTabela(){
+
+
+           }
+           /// <summary>
+           /// Desc:
+           /// Default:
+           /// Nullable:False
+           /// </summary>           
+           public Guid ID {get;set;}
+
+           /// <summary>
+           /// Desc:
+           /// Default:
+           /// Nullable:False
+           /// </summary>           
+           public string Nome {get;set;}
+
+    }
+}
--- Werter.SqlSugarEstudos/Werter.SqlSugarEstudos/Program.cs
+++ Werter.SqlSugarEstudos/Werter.SqlSugarEstudos/Program.cs
@@ -1,12 +1,30 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using SqlSugar;
 
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Werter DEV - Explorando SqlSugar");
+
+var diretorioProjeto = new DirectoryInfo(Environment.CurrentDirectory).Parent.Parent.Parent.FullName;
+var diretorioModelos = Path.Combine(diretorioProjeto, "ModelosGerados");
 
 
 var db = new SqlSugarClient(new ConnectionConfig
 {
     DbType = DbType.SqlServer,
     ConnectionString = "Server=localhost,1433;Database=ExperimentosDotnet;User Id=sa;Password=!123Senha;",
-
+    IsAutoCloseConnection = true,
 });
+
+//var nomeDaTabela = "TB_PrimeiraTabela";
+//var tabelaExiste = db.DbMaintenance.GetTableInfoList().Exists(x => x.Name == nomeDaTabela);
+
+//if (tabelaExiste)
+//    db.DbMaintenance.DropTable(nomeDaTabela);
+
+//db.DbMaintenance.CreateTable(nomeDaTabela, new List<DbColumnInfo>
+//{
+//    new DbColumnInfo { ColumnDescription = "Descrição da coluna", DbColumnName = "ID", DataType = "uniqueidentifier", IsPrimarykey = true },
+//    new DbColumnInfo { ColumnDescription = "Nome de alguma coisa", DbColumnName = "Nome", DataType = "varchar(30)"},
+//});
+
+db.DbFirst.CreateClassFile(diretorioModelos);
+
