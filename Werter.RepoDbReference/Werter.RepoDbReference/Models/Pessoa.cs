3e551f5606b0c9bba9c88aaea4a05218a3e41cbf
werter bonfim <werter@hotmail.com.br>
2022-01-12 14:30:18 -0300

    mais exemplos

---
+++ Werter.RepoDbReference/Werter.RepoDbReference/Models/Pessoa.cs
@@ -0,0 +1,10 @@
+﻿namespace Werter.RepoDbReference.Models
+{
+    public class Pessoa
+    {
+        public Guid Id { get; set; }
+        public string Nome { get; set; }
+        public string Cpf { get; set; }
+        public DateTime DataHoraCadastro { get; set; }
+    }
+}
---
+++ Werter.RepoDbReference/Werter.RepoDbReference/Program.cs
@@ -0,0 +1,32 @@
+﻿using RepoDb;
+using System.Data.SqlClient;
+using Werter.RepoDbReference.Models;
+
+Console.WriteLine("Iniciando estudos RepoDb");
+
+RepoDb.SqlServerBootstrap.Initialize();
+
+
+
+var stringDeConnexao = "Server=localhost,1433;Database=ExperimentosDotnet;User Id=sa;Password=!123Senha";
+using var conexao = new SqlConnection(stringDeConnexao);
+
+for (int i = 0; i < 10; i++)
+{
+    var pessoa = new Pessoa
+    {
+        Id = Guid.NewGuid(),
+        Nome = "Fuleno",
+        Cpf = "11122233344",
+        DataHoraCadastro = DateTime.Now
+    };
+
+    var id = conexao.Insert<Pessoa, Guid>(pessoa);
+
+    Console.WriteLine(id);
+    pessoa.Nome = "Ciclano";
+
+    //conexao.Update<Pessoa>(pessoa, new Field[] { new Field("Nome") });
+    
+    conexao.Update<Pessoa>(pessoa); 
+}
